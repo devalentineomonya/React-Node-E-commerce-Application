c862cc661debff15e2905371069392ec2f92d755
Valentine Omonya <valomosh254@gmail.com>
2024-08-08 21:23:56 +0300

    Added router file

--- client/src/components/common/ProductsLayout/ProductsLayoutHero.jsx
+++ client/src/components/common/ProductsLayout/ProductsLayoutHero.jsx
@@ -6,33 +6,39 @@ import MainLayout from "../MainLayout/MainLayout";
 
 const ProductsLayoutHero = () => {
   useEffect(() => {
+    // Image reveal
     const revealImage = scrollReveal({
-      origin: "bottom",
-      distance: "120px",
-      duration: 500,
-      delay: 100,
-      scale: 0.5,
+      origin: "right",
+      distance: "80px",
+      duration: 800,
+      delay: 200,
+      scale: 0.9,
+      interval: 20,
+      reset: true,
+    });
+
+    // Text reveal
+    const revealText = scrollReveal({
+      origin: "left",
+      distance: "80px",
+      duration: 1200,
+      delay: 300,
+      scale: 0.9,
       interval: 10,
       reset: true,
     });
-    const revealText = scrollReveal({
-      origin: "left",
-      distance: "500px",
+
+    // Button reveal
+    const revealButton = scrollReveal({
+      origin: "bottom",
+      distance: "60px",
       duration: 1000,
       delay: 500,
-      scale: 0.7,
-      interval: 5,
+      scale: 0.95,
+      interval: 15,
       reset: true,
     });
-    const revealButton = scrollReveal({
-      origin: "top",
-      distance: "400px",
-      duration: 500,
-      delay: 1500,
-      scale: 0.7,
-      interval: 5,
-      reset: true,
-    });
+
     revealButton.reveal(".products-hero-button");
     revealText.reveal(".layout-hero-text");
     revealImage.reveal(".products-layout-hero-image");
@@ -43,7 +49,7 @@ const ProductsLayoutHero = () => {
       <div className="products-layout-hero-container">
         <div className="products-layout-hero-text">
           <h1 className="layout-hero-text">
-            Grab upto 50% Off on selected Item
+            Grab up to 50% Off on Selected Items
           </h1>
           <button title="Buy" aria-label="Buy" className="products-hero-button">
             Buy Now
