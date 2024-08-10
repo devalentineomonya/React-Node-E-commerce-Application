fef1b42abc2b07767162e9dd599b433e33df5c8b
Valentine Omonya <valomosh254@gmail.com>
2024-08-10 11:12:03 +0300

    Added Profile page

---
+++ client/src/pages/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "../../components/common/MainLayout/MainLayout"
+import ProfileMain from "../../components/Profile/ProfileMain"
+const Profile = () => {
+  return (
+    <MainLayout >
+      <ProfileMain/>
+    </MainLayout>
+  )
+}
+
+export default Profile
