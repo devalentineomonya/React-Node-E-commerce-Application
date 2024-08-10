3c741df1d596d739055c859de4e2823912a01588
Valentine Omonya <valomosh254@gmail.com>
2024-08-10 11:12:32 +0300

    Added fallback Loading component

---
+++ client/src/components/common/Loading/Loading.jsx
@@ -0,0 +1,15 @@
+import { FaSpinner } from "react-icons/fa";
+import MainLayout from "../MainLayout/MainLayout";
+
+const Loading = () => {
+  return (
+    <MainLayout>
+      <div className="flex justify-center items-center h-[60vh]">
+        <FaSpinner className="animate-spin text-blue-500 text-4xl" />
+        <span className="ml-4 text-lg text-gray-700">Loading...</span>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default Loading;
