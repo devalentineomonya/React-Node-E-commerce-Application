f78d3eb9a7ffc0cbf51e986a665447449cc2ab4b
Valentine Omonya <valomosh254@gmail.com>
2024-08-12 23:12:41 +0300

    Modified user router

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,7 +1,8 @@
 const express = require("express")
-const {registerUser} = require("../controllers/user.controller")
+const {registerUser, getUser} = require("../controllers/user.controller")
 const userRouter = express.Router()
  userRouter.post("/add",registerUser)
+ .get("/get/:userId", getUser)
 
 
  module.exports = userRouter
