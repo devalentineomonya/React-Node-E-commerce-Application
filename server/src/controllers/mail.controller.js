49c8427f9754306bc38ff88964a9857fe3ef4159
Valentine Omonya <valomosh254@gmail.com>
2024-08-12 23:13:53 +0300

    Modified mail controller

--- server/src/controllers/mail.controller.js
+++ server/src/controllers/mail.controller.js
@@ -2,13 +2,27 @@ const nodemailer = require('nodemailer');
 const emailPage = require("../utils/mailPage.util")
 const config = require("../config/config")
 
-const sendVerificationEmail = async (email,userId, verificationCode, token) => {
-    const title = "Verify Your Account"
-    const message = "Your Verification code is:"
-    const description = "Use this code to verify your DevalShopping Cart account. The code is valid for 3 day."
+const sendVerificationEmail = async (email,userId, verificationCode, token,action) => {
+    let title 
+    let message 
+    let description
+    switch (action) {
+        case "verify":
+            title = "Verify Your Account"
+            message = "Your Verification code is:"
+            description = "Use this code to verify your DevalShopping Cart account. The code is valid for 3 day."
+            break;
+    case "reset":
+        title = "Reset Your Password"
+      
+        description = "Use this code to reset your DevalShopping Cart account password. The code is valid for 3 day."
+        default:
+            break;
+    }
 
-    const emailHTML = emailPage(title, message, description, verificationCode, token,userId)
-    console.log(emailHTML)
+    const emailHTML = emailPage(title, message, description, verificationCode, token,userId, action)
+   
+  
 
     const transporter = nodemailer.createTransport({
         service: config.email.service,
@@ -20,7 +34,7 @@ const sendVerificationEmail = async (email,userId, verificationCode, token) => {
     });
 
     const mailOptions = {
-        from: 'DevalShoppingCart|',
+        from: '"DevalShoppingCart" <contact@devalshoppingcart.vercel.app>',
         to: email,
         subject: `DevalShoppingCart | Account Verification - ${verificationCode}` ,
         html: emailHTML
