67a09ae42aa050b1868886c7c3e2fd5d0396b771
Valentine Omonya <valomosh254@gmail.com>
2024-08-12 23:13:21 +0300

    Modified auth middleware

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ success:false, message: 'Authorization token required' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+
+        const decoded = jwt.verify(token, config.jwt.secret);
+
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({success:false, message: 'Invalid or expired token' });
+    }
+};
+
+module.exports = authMiddleware;
