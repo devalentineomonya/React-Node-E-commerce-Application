2bfbaa0859236086832aac9bf0a8dd848e525c89
Valentine Omonya <valomosh254@gmail.com>
2024-08-14 19:31:52 +0300

    Modified OTPform component in client side

--- client/src/components/Auth/OtpForm.jsx
+++ client/src/components/Auth/OtpForm.jsx
@@ -2,31 +2,52 @@ import OtpInput from "./OtpInput";
 import accountVerify from "../../assets/images/accountVerify.png";
 import logo from "../../assets/images/logo_big.png";
 import { FormCancelButton } from "./PasswordReset";
-import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useVerifyWithCodeMutation } from "../../../app/features/auth/authAPI";
-import { setAuthError, setAuthLoading, setUser } from "../../../app/features/auth/authSlice";
 import { useFetchUserDataQuery } from "../../../app/features/user/userAPI";
 import { toast } from "react-toastify";
 import MainLayout from "../common/MainLayout/MainLayout";
+import {
+  useResendVerificationCodeMutation,
+  useVerifyWithCodeMutation,
+} from "../../../app/features/auth/authAPI";
+import {
+  Navigate,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
+import {
+  regenerateVerificationCode,
+  setAuthError,
+  setAuthLoading,
+  setUser,
+} from "../../../app/features/auth/authSlice";
 
 const OtpForm = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [verify, { isLoading: isVerifying }] = useVerifyWithCodeMutation();
-  const user= useSelector((state) => state.auth.user); // Adjust if needed
+  const [resendCode, { isLoading }] = useResendVerificationCodeMutation();
+  const user = useSelector((state) => state.auth.user);
+  const search = useSearchParams();
+  const query = useParams();
+  console.log(search);
+  const token = localStorage.getItem("token");
+  const userId = localStorage.getItem("userId");
 
   const fetchUserData = useFetchUserDataQuery(user?._id, {
     skip: !user?._id,
   });
+  if ((!token && !user && !userId) || user?.isVerified) {
+    return <Navigate to="/" replace />;
+  }
 
   const onOtpSubmit = async (verificationCode) => {
-    console.log(verificationCode)
+    console.log(verificationCode);
     dispatch(setAuthLoading(true));
     try {
       const result = await verify({ verificationCode }).unwrap();
       if (result.success) {
-      
         const newUser = await fetchUserData.refetch().unwrap();
         dispatch(setUser(newUser));
         toast.success(result.message);
@@ -36,7 +57,7 @@ const OtpForm = () => {
       }
     } catch (error) {
       dispatch(setAuthError(error));
-      console.log(error)
+      console.log(error);
       toast.error(error.data.message || error.message);
     } finally {
       dispatch(setAuthLoading(false));
@@ -50,58 +71,85 @@ const OtpForm = () => {
     formData.forEach((value, key) => {
       otpJson[key] = value;
     });
-  
- 
-    const otpValue = Object.values(otpJson).join('');
-   
-  
+
+    const otpValue = Object.values(otpJson).join("");
+
     if (otpValue.length === 6) {
       onOtpSubmit(otpValue);
     } else {
       toast.error("Fill all OTP fields");
     }
   };
+  const resendVerificationCode = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await resendCode().unwrap();
+      await dispatch(regenerateVerificationCode(response));
+      toast.success(response?.message);
+    } catch (error) {
+      toast.error(error?.data?.message);
+    }
+  };
+  console.log(user);
+  // console.log(message)
+  // window.addEventListener("load", () => {
+  //   setTimeout(() => {
+  //     if (message && !user.) {
+  //       toast.error(message);
+  //       alert("Hello world");
+  //       console.log("Loaded")
+  //       console.log(message)
+  //     }
+  //   }, 2000);
+  // });
+
   return (
     <MainLayout overflow>
       <div className="loginsignup">
-    <div className="loginsignup-container">
-      <div className="login-signup-image">
-        <img src={accountVerify} loading="lazy" />
-      </div>
-      <div className="login-signup-form">
-        <div className="login-sign-text">
-          <img src={logo} alt="" />
-          <h2>Verify Account</h2>
-          <p>
-            Ullamco Lorem officia voluptate incididunt consectetur id duis elit
-            ex ex tempor ea mollit cillum.
-          </p>
-        </div>
-        <form onSubmit={onFormSubmit} method="POST">
-          <OtpInput onOtpSubmit={onOtpSubmit} length={6} />
-          <div className="remember-section mt-5">
-            <div className="remember-input"></div>
-            <Link to="/auth/reset-password">Resend Code</Link>
+        <div className="loginsignup-container">
+          <div className="login-signup-image">
+            <img src={accountVerify} loading="lazy" />
           </div>
+          <div className="login-signup-form">
+            <div className="login-sign-text">
+              <img src={logo} alt="" />
+              <h2>Verify Account</h2>
+              <p>
+                Ullamco Lorem officia voluptate incididunt consectetur id duis
+                elit ex ex tempor ea mollit cillum.
+              </p>
+            </div>
+            <form onSubmit={onFormSubmit} method="POST">
+              <OtpInput onOtpSubmit={onOtpSubmit} length={6} />
+              <div className="remember-section mt-5">
+                <div className="remember-input"></div>
+                <button onClick={resendVerificationCode} className="flex ">
+                  {isLoading ? (
+                    <div className="h-5 w-5 rounded-full border-2 border-gray-400 border-t-transparent animate-spin"></div>
+                  ) : (
+                    "Resend Code"
+                  )}
+                </button>
+              </div>
 
-          <div className="login-signup-button mt-4">
-            <button
-              type="submit"
-              className="bg-customGreen text-white hover:bg-black flex justify-center items-center"
-              disabled={isVerifying}
-            >
-              {isVerifying ? (
-                <div className="h-6 w-6 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
-              ) : (
-                "Verify"
-              )}
-            </button>
+              <div className="login-signup-button mt-4">
+                <button
+                  type="submit"
+                  className="bg-customGreen text-white hover:bg-black flex justify-center items-center"
+                  disabled={isVerifying}
+                >
+                  {isVerifying ? (
+                    <div className="h-6 w-6 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
+                  ) : (
+                    "Verify"
+                  )}
+                </button>
+              </div>
+            </form>
+            <FormCancelButton navigate={navigate} />
           </div>
-        </form>
-        <FormCancelButton navigate={navigate} />
+        </div>
       </div>
-    </div>
-    </div>
     </MainLayout>
   );
 };
