695a55eee93af01f5c02184c679f7a10b187592b
Valentine Omonya <valomosh254@gmail.com>
2024-08-14 19:40:18 +0300

    Modified account main component in client side

--- client/src/components/MyAccount/AccountMain.jsx
+++ client/src/components/MyAccount/AccountMain.jsx
@@ -1,28 +1,30 @@
-import { HiOutlineMenuAlt4 } from "react-icons/hi"
-import "../Profile/profile.css"
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import "../Profile/profile.css";
 import { useState } from "react";
 import AccountNav from "./AccountNav";
 import AccountForm from "./AccountForm";
 const AccountMain = () => {
-    const [showProfileNav, setShowProfileNav] = useState(false);
+  const [showProfileNav, setShowProfileNav] = useState(false);
   return (
     <>
-    <div onClick={() => setShowProfileNav((prev) => !prev)}  onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                setShowProfileNav((prev) => !prev)
-              }
-            }} className="inline-block lg:hidden">
-      <HiOutlineMenuAlt4 size={20} />
-    </div>
+      <div
+        onClick={() => setShowProfileNav((prev) => !prev)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            setShowProfileNav((prev) => !prev);
+          }
+        }}
+        className="inline-block lg:hidden"
+      >
+        <HiOutlineMenuAlt4 size={20} />
+      </div>
 
-    <div
-      className="profile-container"
-    >
-    <AccountNav showProfileNav={showProfileNav}/>
-      <AccountForm/>
-    </div>
-  </>
-  )
-}
+      <div className="profile-container">
+        <AccountNav showProfileNav={showProfileNav} />
+        <AccountForm />
+      </div>
+    </>
+  );
+};
 
-export default AccountMain
+export default AccountMain;
