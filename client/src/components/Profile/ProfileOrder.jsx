b112e4b4a6d9b7512daa2079d9ea1e6021b9395c
Valentine Omonya <valomosh254@gmail.com>
2024-08-14 19:40:25 +0300

    Modified Profile order component in client side

--- client/src/components/Profile/ProfileOrder.jsx
+++ client/src/components/Profile/ProfileOrder.jsx
@@ -1,20 +1,35 @@
+import { useSelector } from "react-redux";
 import CartItemCard from "../Cart/CartItemCard";
+import ProfileEmptyPage from "./ProfileEmptyPage";
+import { BsCart4 } from "react-icons/bs";
 
 const ProfileOrder = () => {
+  const user = useSelector((state) => state.auth.user);
   return (
     <div>
       <div className="profile-page-title">
         <h1>Orders</h1>
       </div>
       <div className="w-full p-4 pb-0 flex items-baseline border-b gap-x-3 text-sm font-medium text-gray-600 ">
-        <button className="pb-4 border-b-2 border-customGreen">ONGOING(0)</button>
-        <button className="pb-4 border-b-2 border-customGreen">CANCELLED(0)</button>
+        <button className="pb-4 border-b-2 border-customGreen">
+          ONGOING(0)
+        </button>
+        <button className="pb-4 border-b-2 border-customGreen">
+          CANCELLED(0)
+        </button>
       </div>
-      <div className="cart-items px-4">
-        <CartItemCard/>
-        <CartItemCard/>
-        <CartItemCard/>
-        <CartItemCard/>
+      <div className={`px-4 ${user?.orders?.length > 0 ? "": "flex justify-center items-center mt-12"}`}>
+        {user?.orders?.length > 0 ? (
+          user?.orders?.map((order, i) => <CartItemCard key={i} />)
+        ) : (
+        
+            <ProfileEmptyPage
+              message="You have no placed orders yet"
+              description="All your orders will be saved here for you to access them anytime."
+              icon={<BsCart4 size={50} className="rotate-12 text-orange-400" />}
+            />
+        
+        )}
       </div>
     </div>
   );
