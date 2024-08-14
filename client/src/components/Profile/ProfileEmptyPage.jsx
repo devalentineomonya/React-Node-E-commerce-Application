e239bd4bc3e21915ac2b6b844b4173491c9f81ce
Valentine Omonya <valomosh254@gmail.com>
2024-08-14 19:40:21 +0300

    Modified Profile empty component to accept props

--- client/src/components/Profile/ProfileEmptyPage.jsx
+++ client/src/components/Profile/ProfileEmptyPage.jsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
-
-const ProfileEmptyPage = () => {
+import PropTypes from "prop-types"
+const ProfileEmptyPage = ({message, description, icon}) => {
   return (
     <div className="w-full h-full flex justify-center items-center mt-20 flex-col">
-      <div className="w-24 h-24 rounded-full bg-gray-100  ">
+      <div className="w-24 h-24 rounded-full bg-gray-100  flex justify-center items-center ">
+        {icon}
       </div>
-        <h6 className="mt-4 font-medium text-md text-gray-500">djdhd</h6>
+        <h6 className="mt-4 font-medium text-md text-gray-500">{message}</h6>
         <p className="mt-3 font-medium text-sm text-gray-400">
-          Deserunt consequat adipisicing occaecat labore sit magna sit. Sunt
-          consectetur fugiat eiusmod sint magna irure.
+          {description}
         </p>
         <button className="px-5 py-2 rounded-sm bg-customGreen text-white mt-6 shadow-md"><Link to="/shop">CONTINUE SHOPPING</Link></button>
     </div>
   );
 };
-
+ProfileEmptyPage.propTypes = {
+  message:PropTypes.string.isRequired,
+  description:PropTypes.string.isRequired,
+  icon:PropTypes.node.isRequired,
+}
 export default ProfileEmptyPage;
