f136db01a7e22a33028623c3c21f285b6b47a7cb
Valentine Omonya <valomosh254@gmail.com>
2025-01-02 03:50:55 +0300

    Added popular products

---
+++ devalShoppingCart/screens/home/widgets/PopularProducts.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SectionLayout from '@/components/common/layouts/SectionLayout'
+import ScrollCarousel from '@/components/global/ScrollCarousel'
+import ProductCard from '@/components/shared/ProductCard/ProductCard'
+const PopularProducts = () => {
+  return (
+    <SectionLayout title="Weekly Popular Products"  overflow>
+        <ScrollCarousel>
+            <ProductCard />
+        </ScrollCarousel>
+        </SectionLayout>
+  )
+}
+
+export default PopularProducts
