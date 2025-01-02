1723a97aaac0b7bb902c287f795e77392bbb94a5
Valentine Omonya <valomosh254@gmail.com>
2025-01-02 03:49:10 +0300

    Added moved Categories

--- devalShoppingCart/screens/home/widgets/TopCategories.tsx
+++ devalShoppingCart/screens/home/widgets/TopCategories.tsx
@@ -1,84 +1,43 @@
 "use client";
-import React from "react";
 import SectionLayout from "@/components/common/layouts/SectionLayout";
 import CategoryCard from "../components/CategoryCard";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination'; // Import pagination styles
-import { Pagination } from 'swiper/modules';
-import { motion } from 'framer-motion';
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+// import "./bullets.css";
+import { motion } from "framer-motion";
+import { Navigation } from "swiper/modules";
 const TopCategories: React.FC = () => {
-
-  const variants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0 },
-  };
-
   return (
-    <SectionLayout title="Top Categories">
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={variants}
-        transition={{ duration: 0.5 }}
-        className="flex justify-between w-full gap-x-3"
-      >
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
+    >
+      <SectionLayout overflow  title="Top Categories">
         <Swiper
-          spaceBetween={20}
-          slidesPerView={6}
-          pagination={{
-            // bulletActiveClass: "swiper-pagination-bullet-active",
-            clickable: true,
-            renderBullet: (className) => {
-              return `<span class="${className} w-2 h-2 bg-gray-400 rounded-full mx-1 transition-all duration-300 ease-in-out"></span>`;
-            },
-          }}
-          modules={[Pagination]}
+        modules={[Navigation]}
+          spaceBetween={16}
+          slidesPerView={1}
           breakpoints={{
-            0: {
-              slidesPerView: 1,
-              spaceBetween: 10,
-            },
-            576: {
-              slidesPerView: 2,
-              spaceBetween: 10,
-            },
-            768: {
-              slidesPerView: 3,
-              spaceBetween: 15,
-            },
-            1024: {
-              slidesPerView: 4,
-              spaceBetween: 20,
-            },
-            1280: {
-              slidesPerView: 6,
-              spaceBetween: 20,
-            },
+            640: { slidesPerView: 2 },
+            1024: { slidesPerView: 4 },
+            1280: { slidesPerView: 5 },
+          }}
+          pagination={{
+            clickable: true,
+            bulletClass: "swiper-pagination-bullet",
+            bulletActiveClass: "swiper-pagination-bullet-active",
           }}
         >
-          <SwiperSlide>
-            <CategoryCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CategoryCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CategoryCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CategoryCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CategoryCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <CategoryCard />
-          </SwiperSlide>
+          {[...Array(5)].map((_, index) => (
+            <SwiperSlide key={index}>
+              <CategoryCard />
+            </SwiperSlide>
+          ))}
         </Swiper>
-      </motion.div>
-    </SectionLayout>
+      </SectionLayout>
+    </motion.div>
   );
 };
 
