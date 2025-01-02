c821fc23f7af7ab24144ec930636bc3dbc764316
Valentine Omonya <valomosh254@gmail.com>
2025-01-02 03:51:49 +0300

    Added Best Selling products

---
+++ devalShoppingCart/screens/home/widgets/BestSelling.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SectionLayout from '@/components/common/layouts/SectionLayout'
+import ScrollCarousel from '@/components/global/ScrollCarousel'
+import ProductCard from '@/components/shared/ProductCard/ProductCard'
+const BestSelling = () => {
+  return (
+    <SectionLayout title="Best Selling Products for you!"  overflow>
+        <ScrollCarousel>
+            <ProductCard />
+        </ScrollCarousel>
+        </SectionLayout>
+  )
+}
+
+export default BestSelling
