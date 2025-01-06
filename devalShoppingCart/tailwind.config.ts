fd39fa6c9c44a46b6ba76c83654fff79e4c29a7c
Valentine Omonya <valomosh254@gmail.com>
2025-01-06 20:38:53 +0300

    Added global styles

--- devalShoppingCart/tailwind.config.ts
+++ devalShoppingCart/tailwind.config.ts
@@ -10,11 +10,23 @@ export default {
   theme: {
     extend: {
       colors: {
-
-            primary:"#003d29"
-
+        primary: "#003d29",
+      },
+      animation: {
+        meteor: "meteor 5s linear infinite",
+      },
+      keyframes: {
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX('0')", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
       },
     },
   },
+
   plugins: [],
 } satisfies Config;
