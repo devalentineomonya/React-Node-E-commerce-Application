89da877563fded85381085d3f23d39f5e9644ce7
Valentine Omonya <valomosh254@gmail.com>
2025-01-06 20:35:25 +0300

    Added navigation progress indicator

--- devalShoppingCart/app/layout.tsx
+++ devalShoppingCart/app/layout.tsx
@@ -1,14 +1,20 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import NavbarMain from "@/components/common/navbar/NavbarMain";
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 import Footer from "@/components/common/footer/Footer";
-import  {QueryProvider} from "@/providers/query-provider"
-const inter = Inter({ subsets: ['latin'] })
+import { QueryProvider } from "@/providers/query-provider";
+import { ProgressBarProviders } from "@/providers/progress-bar-provider";
+
+import { Bounce, ToastContainer } from "react-toastify";
+import Script from "next/script";
+
+const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
-    title: "DevalShoppingCart | Excellent Shopping Experience",
-    description: "Discover unbeatable deals and a seamless shopping experience at DevalShoppingCart. Shop a wide range of high-quality products, enjoy secure payments, and fast shipping. Join our community of satisfied customers and elevate your shopping experience today!"
-  };
+  title: "DevalShoppingCart | Excellent Shopping Experience",
+  description:
+    "Discover unbeatable deals and a seamless shopping experience at DevalShoppingCart. Shop a wide range of high-quality products, enjoy secure payments, and fast shipping. Join our community of satisfied customers and elevate your shopping experience today!",
+};
 
 export default function RootLayout({
   children,
@@ -17,14 +23,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
+      <body className={`${inter.className} antialiased`}>
         <QueryProvider>
-        <NavbarMain/>
-                {children}
-        <Footer/>
+          <ProgressBarProviders>
+            <NavbarMain />
+            {children}
+            <Footer />
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+              transition={Bounce}
+            />
+          </ProgressBarProviders>
         </QueryProvider>
+        <Script
+          src="https://accounts.google.com/gsi/client"
+          strategy="beforeInteractive"
+        />
       </body>
     </html>
   );
