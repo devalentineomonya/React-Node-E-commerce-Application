00a3e80adc8f25d3896a5f5ae939a47933561344
Valentine Omonya <valomosh254@gmail.com>
2025-01-09 22:34:32 +0300

    Add new deals feature

---
+++ devalShoppingCart/app/(apps)/(public)/deals/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ProductsLayout from '@/components/common/layouts/products/ProductsLayout'
+const Deals = () => {
+  return (
+   <ProductsLayout/>
+  )
+}
+
+export default Deals
